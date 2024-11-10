2bba2075e2a7ed2432c75424498c724a3ac1df98
Arthur Samuel Hack <arthur.hack.live@live.com>
2024-11-10 20:16:44 +0000

    Added prisma ORM integration and input dto validation

--- src/categories/dto/create-category.dto.ts
+++ src/categories/dto/create-category.dto.ts
@@ -1,1 +1,12 @@
-export class CreateCategoryDto {}
+import { IsNotEmpty, IsOptional, IsString, MaxLength } from "class-validator";
+
+export class CreateCategoryDto {
+    @MaxLength(255, { message: "O nome precisa ser menor ou igual a 255 caracteres" })
+    @IsString({ message: "O nome precisa ser uma string" })
+    @IsNotEmpty({ message: "O nome não pode estar vazio" })
+    name: string;
+
+    @IsString({ message: "A descrição precisa ser uma string" })
+    @IsOptional()
+    description?: string | null;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaExceptionFilter } from './exception.filters/prisma.exception-filter';
+import { CatchAllErrorsExceptionFilter } from './exception.filters/catch-all-errors.exception-filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(
+    new CatchAllErrorsExceptionFilter(httpAdapter),
+    new PrismaExceptionFilter(),
+  );
+  app.useGlobalPipes(
+    new ValidationPipe({
+      errorHttpStatusCode: 422
+    })
+  )
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
